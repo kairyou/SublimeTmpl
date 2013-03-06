670cdc960273e7b77cd22721563969a979204311
leon <xhl@live.it>
2013-03-07 02:31:16 +0800

    + support ST3 3016

--- sublime-tmpl.py
+++ sublime-tmpl.py
@@ -1,22 +1,24 @@
 # + Python 3 support
 # + sublime text 3 support
-import sublime
-import sublime_plugin
-import os
+
+import sublime, sublime_plugin
+import sys, os
+
+import zipfile
 
 PACKAGE_NAME = 'SublimeTmpl'
 TMLP_DIR = 'templates'
 KEY_SYNTAX = 'syntax'
 KEY_FILE_EXT = 'extension'
 
-base_path = os.path.abspath(os.path.dirname(__file__))
-packages_path = sublime.packages_path() or os.path.dirname(base_path)
+BASE_PATH = os.path.abspath(os.path.dirname(__file__)) #Installed Packages/xx.sublime-package
+PACKAGES_PATH = sublime.packages_path() # for ST2
 
-is_gte_st3 = int(sublime.version()[0]) >= 3
+# sys.path += [BASE_PATH]
+# sys.path.append(BASE_PATH)
+# import sys;print(sys.path)
 
-# TARGET_PATH = os.path.join(packages_path, PACKAGE_NAME)
-# if not os.path.isdir(TARGET_PATH):
-#     os.makedirs(TARGET_PATH)
+IS_GTE_ST3 = int(sublime.version()[0]) >= 3
 
 class SublimeTmplCommand(sublime_plugin.TextCommand):
 
@@ -42,8 +44,8 @@ class SublimeTmplCommand(sublime_plugin.TextCommand):
         code = ''
         file_name = type + '.tmpl'
         isIOError = False
-        # print(packages_path)
-        if is_gte_st3:
+        # print(PACKAGES_PATH)
+        if IS_GTE_ST3:
             self.tmpl_path = 'Packages/' + PACKAGE_NAME + '/' + TMLP_DIR + '/' + file_name
             try:
                code = sublime.load_resource(self.tmpl_path)
@@ -52,7 +54,7 @@ class SublimeTmplCommand(sublime_plugin.TextCommand):
                 isIOError = True
         else:
             self.tmpl_path = os.path.join(PACKAGE_NAME, TMLP_DIR, file_name)
-            file = os.path.join(packages_path, self.tmpl_path)
+            file = os.path.join(PACKAGES_PATH, self.tmpl_path)
             if not os.path.exists(file):
                 isIOError = True
             else:
@@ -85,3 +87,43 @@ class SublimeTmplCommand(sublime_plugin.TextCommand):
         # print(opts[KEY_FILE_EXT])
         if KEY_FILE_EXT in opts:
             v.settings().set('default_extension', opts[KEY_FILE_EXT])
+
+# for ST3 >= 3016
+def plugin_loaded():
+    # global PACKAGES_PATH
+    PACKAGES_PATH = sublime.packages_path()
+    TARGET_PATH = os.path.join(PACKAGES_PATH, PACKAGE_NAME)
+    # print(BASE_PATH, os.path.dirname(BASE_PATH))
+    # print(TARGET_PATH)
+
+    # first run
+    if not os.path.isdir(TARGET_PATH):
+        os.makedirs(os.path.join(TARGET_PATH, TMLP_DIR))
+        # copy user files
+        tmpl_dir = TMLP_DIR + '/'
+        file_list = [
+        'Default.sublime-commands', 'Main.sublime-menu',
+        # if don't copy .py, ST3 throw an exception: ImportError: No module named
+        'sublime-tmpl.py', 
+        'README.md',
+        tmpl_dir + 'css.tmpl', tmpl_dir + 'html.tmpl', tmpl_dir + 'js.tmpl',
+        tmpl_dir + 'php.tmpl', tmpl_dir + 'python.tmpl', tmpl_dir + 'ruby.tmpl',
+        tmpl_dir + 'xml.tmpl'
+        ]
+        try:
+            extract_zip_resource(BASE_PATH, file_list, TARGET_PATH)
+        except Exception as e:
+            print(e)
+
+def extract_zip_resource(path_to_zip, file_list, extract_dir=None):
+    if extract_dir is None:
+        return
+    # print(extract_dir)
+    if os.path.exists(path_to_zip):
+        z = zipfile.ZipFile(path_to_zip, 'r')
+        for f in z.namelist():
+            # if f.endswith('.tmpl'):
+            if f in file_list:
+                # print(f)
+                z.extract(f, extract_dir)
+        z.close()
